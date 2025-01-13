21cc4afd722e0786f3305219148ca8db33fed4f6
Peter Hahlweg <peter.github@mailbox.org>
2025-01-13 23:59:30 +0100

    rework render ast example

--- render_ast.py
+++ render_ast.py
@@ -1,10 +1,9 @@
 from tinygrad.codegen.kernel import Kernel
-from tinygrad.device import Device
 from tinygrad.ops import UOp, Ops, KernelInfo
 from tinygrad.dtype import dtypes
 from tinygrad.shape.shapetracker import ShapeTracker, View
 
-# Your UOps AST
+# AST from UOps
 ast_r_64_32_16_3_4_3_3_3 = UOp(Ops.SINK, dtypes.void, arg=KernelInfo(local_dims=2, upcasted=4, dont_use_locals=False), src=(
  UOp(Ops.STORE, dtypes.void, arg=None, src=(
    UOp(Ops.DEFINE_GLOBAL, dtypes.float.ptr(518400), arg=0, src=()),
@@ -18,12 +17,9 @@ ast_r_64_32_16_3_4_3_3_3 = UOp(Ops.SINK, dtypes.void, arg=KernelInfo(local_dims=
          UOp(Ops.DEFINE_GLOBAL, dtypes.float.ptr(9), arg=2, src=()),
          UOp(Ops.VIEW, dtypes.void, arg=ShapeTracker(views=(View(shape=(64, 32, 16, 3, 4, 3, 3, 3), strides=(0, 0, 0, 0, 0, 1, 0, 3), offset=0, mask=((0, 60), (0, 15), (0, 16), (0, 3), (0, 4), (0, 3), (0, 3), (0, 3)), contiguous=False),)), src=()),)),)),)),)),))
 
-# Create kernel from AST
-kernel = Kernel(ast)
 
 # Get the unoptimized program
+kernel = Kernel(ast_r_64_32_16_3_4_3_3_3)
 program = kernel.to_program()
-
-# Print the kernel code
 print("Unoptimized Kernel Code for r_64_32_16_3_4_3_3_3:")
 print(program.src)
